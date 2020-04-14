c9f9139a4cdfb75d22c2a7769660b469a2456662
Bearnies <longvikhang@gmail.com>
2020-04-15 06:27:22 +0700

    Connected to server and get coordinates

--- app/src/main/java/com/example/dinhvi/MapsActivity.java
+++ app/src/main/java/com/example/dinhvi/MapsActivity.java
@@ -2,8 +2,16 @@ package com.example.dinhvi;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.net.Network;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -11,9 +19,17 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+
+    public double latitude;
+    public double longitude;
+    public String targetName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,13 +51,44 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    public void getRequestQueueForMap() {
+        RequestQueue requestQueue;
+        String urlRequest = "https://dacnpm-backend.herokuapp.com/users/5e92c0641c9d44000027dae1/getchildrenping";
+        JsonObjectRequest stringRequest = new JsonObjectRequest(Request.Method.GET, urlRequest, (String) null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            latitude = Double.parseDouble(response.getString("latitude"));;
+                            longitude = Double.parseDouble(response.getString("longitude"));
+                            targetName = response.getString("children");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MapsActivity.this, "Can not get coordinates !", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
+
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        //GET the location from server
+        getRequestQueueForMap();
+
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng trackingTarget = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(trackingTarget).title(targetName));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(trackingTarget));
+        //mMap.animateCamera(CameraUpdateFactory.zoomTo(30));
     }
 }
